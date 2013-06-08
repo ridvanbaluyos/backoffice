748ac6445910e3deb5509064f6db08fbe3356791
ridvanbaluyos <ridvan@baluyos.net>
2013-06-09 01:42:01 +0800

    Added calculation for leave duration with half day (am/pm). Added Summary Details display.

--- application/views/hr/leave.blade.php
+++ application/views/hr/leave.blade.php
@@ -3,25 +3,59 @@
 <script type="text/javascript">
 $(document).ready(function(namespace)
 {
+	$('#leave_type').change(function (){
+		if ($('#leave_type').val() !== '')
+		{
+			$('#leave_summary_type').html($('#leave_type option:selected').text());
+		}
+	});
+
 	$('#leave_startdate').datepicker()
 		.on('changeDate', function(ev) {
-			if ($('#leave_enddate').val() !== '')
-			{
-				var start_date = $('#leave_startdate').child.val();
-				var end_date = $('#leave_enddate').val();
-			}
+			calculateLeaveDates();
 		
 	});
+
 	$('#leave_enddate').datepicker()
 		.on('changeDate', function(ev) {
-			if ($('#leave_startdate').val() !== '')
-			{
-				var start_date = $('#leave_startdate').val();
-				var end_date = $('#leave_enddate').val();
-				alert(start_date);
-			}
+			calculateLeaveDates();
 		
 	});
+
+	$('#start_meridian').change(function () {
+		calculateLeaveDates();
+	});
+
+	$('#end_meridian').change(function () {
+		calculateLeaveDates();
+	});
+
+	function calculateLeaveDates()
+	{
+		// Start Date
+		var startdate = $("#leave_startdate").datepicker('getDate');
+
+		// End Date
+		var enddate = $("#leave_enddate").datepicker('getDate');
+
+		// Total Leave Days
+		var diff = (enddate - startdate) / (24*3600*1000);
+		diff++;
+
+		if ($('#start_meridian').val() === "pm")
+		{
+			diff = diff - 0.5;
+		}
+
+		if ($('#end_meridian').val() === "am") 
+		{
+			diff = diff - 0.5;
+		}
+
+		$('#leave_duration').html(diff);
+		console.log(diff);
+
+	}
 });
 </script>
 <fieldset>
@@ -32,15 +66,14 @@ $(document).ready(function(namespace)
 			<div class="control-group">
 				<label class="control-label" for="leave_type">Type:</label>
 				<div class="controls">
-					<label class="radio inline">
-					  	<input type="radio" name="leave_type" id="leave_sick" value="sick" />Sick
-					</label>
-					<label class="radio inline">
-						<input type="radio" name="leave_type" id="leave_vacation" value="vacation" />Vacation
-					</label>
-					<label class="radio inline">
-						<input type="radio" name="leave_type" id="leave_emergency" value="emergency" />Emergency
-					</label>
+					<select id="leave_type" name="leave_type">
+				  		<option value="">[Leave Type]</option>
+				 		<option value="sl">Sick Leave</option>
+						<option value="vl">Vacaton Leave</option>
+						<option value="el">Emergency Leave</option>
+						<option value="sl_np">Sick Leave (w/o Pay)</option>
+						<option value="vl_np">Vacaton Leave (w/o Pay)</option>						  	
+					</select>
 				</div>
 			</div>
 			<div class="control-group">
@@ -50,10 +83,10 @@ $(document).ready(function(namespace)
 						<input class="span2" size="16" type="text" value="" readonly>
 						<span class="add-on btn-info"><i class="icon-calendar"></i></span>
 				  	</div>
-				  	<select class="span1">
+				  	<select id="start_meridian" name="start_meridian" class="span1">
 				  		<option>--</option>
-				 		<option>AM</option>
-						<option>PM</option>								  	
+				 		<option value="am">AM</option>
+						<option value="pm">PM</option>								  	
 					</select>
 				</div>
 			</div>
@@ -64,24 +97,35 @@ $(document).ready(function(namespace)
 						<input class="span2" size="16" type="text" value="" readonly>
 						<span class="add-on btn-info"><i class="icon-calendar"></i></span>
 				  	</div>
-				  	<select class="span1">
+				  	<select id="end_meridian" name="end_meridian" class="span1">
 				  		<option>--</option>
-				 		<option>AM</option>
-						<option>PM</option>								  	
+				 		<option value="am">AM</option>
+						<option value="pm">PM</option>								  	
 					</select>
 				</div>
 			</div>
 			<div class="control-group">
 				<label class="control-label" for="leave_reason">Reason(s):</label>
 				<div class="controls">
-					<input type="text" class="span5" id="leave_reason" placeholder="" />
+					<input type="text" class="span4" id="leave_reason" placeholder="" />
 				</div>
 			</div>
 		</div>
-		<div class="span4 offset1">
+		<div class="span4 well">
 			<h4>Summary Details</h4>
-			<p class="text-info">Remaining Leaves:</p>
-				asdf
+			<dl class="dl-horizontal">
+			  	<dt class="text-info">Sick Leaves:</dt>
+			  	<dd>8/15 remaining</dd>
+			  	<dt class="text-info">Vacation Leaves:</dt>
+			  	<dd>8/15 remaining</dd>
+			  	<hr class="divider-horizontal" />
+			  	<dt class="text-success">Leave Type:</dt>
+			  	<dd id="leave_summary_type">Vacation Leave</dd>
+			  	<dt class="text-success">Total # of Days:</dt>
+			  	<dd id="leave_summary_duration">0</dd>
+			  	<dt class="text-success">Approver:</dt>
+			  	<dd>Mylene Pascual</dd>
+			</dl>
 			
 		</div>
 	</div>
@@ -89,7 +133,7 @@ $(document).ready(function(namespace)
 		<div class="control-group">
 				<label class="control-label" for=""></label>
 				<div class="controls">
-					<button type="submit" class="btn btn-small btn-danger">Submit</button>
+					<button type="submit" class="btn btn-small btn-success">Submit</button>
 					<button type="button" class="btn btn-small" onclick="location.href='/human-resources/leaves';">Cancel</button>
 				</div>
 			</div>
